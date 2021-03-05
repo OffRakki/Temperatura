e10e5c6326d80d46c763c95da3d65415fea6daef
RakkiUwU <fernandomarques1505@gmail.com>
2021-03-05 11:05:59 -0300

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
 
     println!("Would you like to convert from (1)°F to °C or (2)°C to °F");
 
+    //Pega o input do teclado e garante que será um número
     let scale: u8 = {
         let mut buffer = String::new();
         io::stdin()
@@ -23,6 +24,7 @@ fn main() {
 fn f() {
     println!("Type your value in °F:");
 
+    //Pega o input do teclado e garante que será um número
     let f: f32 = {
         let mut buffer = String::new();
         io::stdin()
@@ -31,14 +33,17 @@ fn f() {
         buffer.trim().parse().expect("Please, just numbers!")
     };
 
+    // Faz o cálculo para transformar °F em °C
     let result: f32 = (f - 32.0) * 5.0/9.0;
 
+    // Mostra o resultado
     println!("{}°F = {}°C", f, result);
 }
 
 fn c() {
     println!("Type your value in °C:");
 
+    // Pega o input do teclado e garante que será um número
     let c: f32 = {
         let mut buffer = String::new();
         io::stdin()
@@ -47,8 +52,10 @@ fn c() {
         buffer.trim().parse().expect("Please, just numbers!")
     };
 
+    // Faz o cálculo para transformar °C em °F
     let result: f32 = (c * 9.0/5.0) + 32.0;
 
+    //Mostra o resultado
     println!("{}°C = {}°F", c, result);
 }
 
